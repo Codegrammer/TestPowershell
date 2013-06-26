d63518f0cb30c9159b83a1ca2e6f4adcfd113b6f
aboriskin <Alexander.Boriskin@dataart.com>
2013-06-26 19:28:31 +0400

    Add test project.

---
+++ Test/Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args!= null && args.Length == 1)
+            {
+                Console.WriteLine("This is a text from the program");
+            }
+            else
+            {
+                throw new Exception("This is the exception");
+            }
+        }
+    }
+}
